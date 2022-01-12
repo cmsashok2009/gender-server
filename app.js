018f58bf6b3c14c32a1f8f2b3e1043343c593b5f
ashok.kumar086 <ashokkumar.natarajan@samrantech.com>
2022-01-12 16:27:54 +0530

    updated DB

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const { fetchGender } = require("./utils/helper");
 
 const app = express();
 
+app.use(cors());
+
 app.get(`/checkgender/:uname`, fetchGender);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+
+//app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", req.get("Origin") || "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+//   res.header("Access-Control-Expose-Headers", "Content-Length");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Accept, Authorization, Content-Type, X-Requested-With, Range"
+//   );
+//   if (req.method === "OPTIONS") {
+//     return res.send(200);
+//   } else {
+//     return next();
+//   }
+// });
--- utils/helper.js
+++ utils/helper.js
@@ -1,9 +1,21 @@
 const axios = require("axios");
 
+require("dotenv").config();
 const redis = require("redis");
 
 const client = redis.createClient({
-  port: 6379,
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+
+// const client = redis.createClient({
+//   port: 6379,
+// });
+
+client.on("connect", () => {
+  console.log("Connected to our redis instance!");
+  client.set("sample data", "hello world");
 });
 
 client.on("error", (error) => {
@@ -22,35 +34,35 @@ const fetchGender = async (req, res, next) => {
 
     //check the data on redis store.
 
-    client.hget(hashKey, uname, async (_, recipe) => {
-      if (recipe) {
+    client.hget(hashKey, uname, async (_, result) => {
+      if (result) {
         //send the response from cache
 
         return res.send({
           success: true,
-          message: JSON.parse(recipe),
+          message: JSON.parse(result),
           meta_data: "from cache",
         });
       } else {
         //fetch the data.
 
-        const recipe = await axios
+        const result = await axios
           .get(`https://api.genderize.io/?name=${uname}`)
           .catch(console.log);
 
         //set the data on cache
 
-        client.hset(hashKey, uname, JSON.stringify(recipe.data));
+        client.hset(hashKey, uname, JSON.stringify(result.data));
 
         //set the duration of cache.
 
-        client.expire(hashKey, 1440);
+        client.expire(hashKey, 60);
 
         //send the response
 
         return res.send({
           success: true,
-          message: recipe.data,
+          message: result.data,
           meta_data: "from server",
         });
       }
